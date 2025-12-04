885c9c7bfcc9a20cb365f1b765ca064071858ff2
Sooria <115650725+Sooria16@users.noreply.github.com>
2025-12-04 17:58:38 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+app.use(cors());
 app.use(express.json());
 
-app.post("/hello", (req, res) => {
-  const { name } = req.body;
-  res.json({ message: `Hello ${name}, backend API working successfully!` });
+// GET API for your frontend
+app.get("/hello", (req, res) => {
+    const name = req.query.name || "Guest";
+    res.send(`Hello ${name}, backend API working successfully!`);
 });
 
 app.listen(3000, () => console.log("Backend running on port 3000"));
